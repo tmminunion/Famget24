ffcd3c7b2b0060f98ce4c0a85785a525daa32b0b
tmmin <tmminunion@gmail.com>
2024-10-26 12:54:19 +0700

    hudash asshhas

--- fotobingkai/databas.js
+++ fotobingkai/databas.js
@@ -1,4 +1,4 @@
- const firebaseConfig = {
+const firebaseConfig = {
   apiKey: "AIzaSyA7kHlC_5RxBy7g5JbFuYWjGOZ393S0-hk",
   authDomain: "nufat-eltijany.firebaseapp.com",
   databaseURL:
@@ -24,95 +24,73 @@ const googleSignInBtn = document.getElementById("googleSignInBtn");
 googleProvider.setCustomParameters({
   login_hint: "user@example.com",
 });
-googleSignInBtn.addEventListener("click", () => {
-  firebase
-    .auth()
+
+// Fungsi untuk sign in dengan Google
+function signInWithGoogle() {
+  auth
     .signInWithPopup(googleProvider)
     .then((result) => {
-      /** @type {firebase.auth.OAuthCredential} */
-      var credential = result.credential;
-
-      // This gives you a Google Access Token. You can use it to access the Google API.
-      var token = credential.accessToken;
-      // The signed-in user info.
       var user = result.user;
-        // Simpan informasi pengguna ke localStorage
-              localStorage.setItem("displayName", user.displayName);
-              localStorage.setItem("email", user.email);
-              localStorage.setItem("photoURL", user.photoURL);
-      // IdP data available in result.additionalUserInfo.profile.
-      // ...
+      console.log("User logged in using Google:", user);
+
+      // Simpan informasi pengguna ke localStorage
+      localStorage.setItem("displayName", user.displayName);
+      localStorage.setItem("email", user.email);
+      localStorage.setItem("photoURL", user.photoURL);
+      localStorage.setItem("uid", user.uid); // Simpan UID pengguna
+
+      // Tampilkan informasi pengguna
+      updateUIAfterLogin(user);
     })
     .catch((error) => {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      // The email of the user's account used.
-      var email = error.email;
-      // The firebase.auth.AuthCredential type that was used.
-      var credential = error.credential;
-      // ...
+      console.error("Error during sign in:", error);
     });
-});
-firebase.auth().onAuthStateChanged((user) => {
-  if (user) {
-    // Cek metode login pengguna
-    var providerData = user.providerData;
+}
 
-    // Cek apakah pengguna login melalui Google
-    var isGoogleProvider = providerData.some(
+// Fungsi untuk memperbarui UI setelah login
+function updateUIAfterLogin(user) {
+  if (user) {
+    googleSignInBtn.style.display = "none"; // Sembunyikan tombol login
+    $("#rownama").text(user.displayName); // Tampilkan nama pengguna di UI
+
+    // Simpan informasi pengguna ke localStorage
+    localStorage.setItem("displayName", user.displayName);
+    localStorage.setItem("email", user.email);
+    localStorage.setItem("photoURL", user.photoURL);
+    localStorage.setItem("uid", user.uid); // Simpan UID pengguna
+
+    console.log("User Display Name: ", user.displayName);
+    console.log("User Email: ", user.email);
+    console.log("User Photo URL: ", user.photoURL);
+    console.log("User UID: ", user.uid); // Tambahkan log UID pengguna
+  }
+}
+
+// Cek status login pengguna
+auth.onAuthStateChanged((user) => {
+  if (user) {
+    // Pengguna sudah login
+    console.log("User is logged in:", user);
+
+    // Periksa apakah login melalui Google
+    var isGoogleProvider = user.providerData.some(
       (provider) => provider.providerId === "google.com"
     );
 
     if (isGoogleProvider) {
-      // Pengguna login melalui Google, izinkan akses
-      console.log("User is logged in using Google:", user);
-const InBtn = document.getElementById("konlog");
-  googleSignInBtn.style.display  ="none";      // Simpan informasi pengguna ke localStorage
-      localStorage.setItem("displayName", user.displayName);
-      localStorage.setItem("email", user.email);
-      localStorage.setItem("photoURL", user.photoURL);
-$("#rownama").text(user.displayName);
-// document.getElementById('rownama').innerText=user.displayName;  
-      // Tampilkan informasi pengguna
-      console.log("User Display Name: ", user.displayName);
-      console.log("User Email: ", user.email);
-      console.log("User Photo URL: ", user.photoURL);
-      console.log("User Email Verified: ", user.emailVerified);
-      console.log("User UID: ", user.uid);
+      // Pengguna login melalui Google
+      updateUIAfterLogin(user);
     } else {
-      // Pengguna tidak login dengan Google, lakukan logout
+      // Jika bukan login dari Google, bisa tambahkan logika lain atau logout
       console.log("User is not logged in using Google. Logging out...");
-
-    
+      auth.signOut(); // Logout otomatis jika diperlukan
     }
   } else {
-    // No user is signed in, trigger Google sign-in popup
-  
-    firebase
-      .auth()
-      .signInWithPopup(googleProvider)
-      .then((result) => {
-        // User signed in
-        var user = result.user;
-        console.log("User logged in using Google:", user);
+    // Pengguna belum login, trigger popup login Google secara otomatis
+    console.log("No user is signed in. Triggering Google sign-in...");
 
-        // Simpan informasi pengguna ke localStorage
-        localStorage.setItem("displayName", user.displayName);
-        localStorage.setItem("email", user.email);
-        localStorage.setItem("photoURL", user.photoURL);
-
-        // Tampilkan informasi pengguna
-        console.log("User Display Name: ", user.displayName);
-        console.log("User Email: ", user.email);
-        console.log("User Photo URL: ", user.photoURL);
-        console.log("User Email Verified: ", user.emailVerified);
-        console.log("User UID: ", user.uid);
-      })
-      .catch((error) => {
-        console.error("Error during sign in:", error);
-      });
-      
+    // Langsung jalankan fungsi sign-in seolah-olah tombol diklik
+    signInWithGoogle();
   }
 });
 
@@ -134,61 +112,78 @@ function kirimdatta(base64Image) {
     });
 }
 
-function kirimdata6h6h6h(base64Imagena) {
-  const clientId = "082eb55cc144305"; // Ganti dengan Client ID kamu
+function kirimdata(base64Imagena) {
+  const apikeyana = [
+    "025ba3ace62a66d",
+    "082eb55cc144305",
+    "70519e36199385e",
+    "8d04e5606260801",
+    "a692f82b1aced47",
+    "b4b4301cdb7c9c8",
+    "7c0326461ff3839",
+    "f22aac7a4a746bc",
+    "de75765ef3b4199",
+  ];
+  const randomIndex = Math.floor(Math.random() * apikeyana.length);
+  const clientId = apikeyana[randomIndex]; // Ganti dengan Client ID kamu
 
   // Pisahkan base64 menjadi data biner (tanpa header MIME)
   const base64Image = base64Imagena.split(",")[1];
   const displayName = localStorage.getItem("displayName");
   const email = localStorage.getItem("email");
   const photoURL = localStorage.getItem("photoURL"); // Ambil URL foto profil
+  const Userid = localStorage.getItem("uid"); // Ambil URL foto profil
   var myHeaders = new Headers();
   myHeaders.append("Authorization", "Client-ID " + clientId);
 
   var formdata = new FormData();
-  formdata.append("base64data", base64Image);
+  formdata.append("image", base64Image);
   formdata.append("type", "base64");
   formdata.append("title", "Image upload by " + displayName); // Gunakan displayName sebagai judul
-  formdata.append(
-    "description",
-    `Uploaded by ${displayName} (${email}). Profile Picture: ${photoURL}`
-  ); // Gunakan displayName, email, dan photoURL di deskripsi
+  formdata.append("description", `Uploaded by ${displayName}`); // Gunakan displayName, email, dan photoURL di deskripsi
 
-//var link  = "https://twibone.bungtemin.net/upload.php";
-var link  = "https://api.imgur.com/3/upload";
   var requestOptions = {
     method: "POST",
     headers: myHeaders,
     body: formdata,
     redirect: "follow",
   };
-  fetch(link, requestOptions)
+  fetch("https://api.imgur.com/3/upload", requestOptions)
     .then((response) => response.json())
     .then((result) => {
-      alert("resul");
+      console.log(result);
       if (result.success) {
-        alert("success");
         console.log("Image uploaded successfully: ", result.data.link);
 
         // Simpan URL gambar ke Firestore
         db.collection("imageskoleksi")
           .add({
+            id: result.data.id,
             imageUrl: result.data.link,
             title: result.data.title,
+            height: result.data.height,
+            width: result.data.width,
+            deletehash: result.data.deletehash,
+            datetime: result.data.datetime,
             description: result.data.description,
             uploadedAt: new Date(),
+            Userid: Userid,
+            love: 0,
             uploadedBy: displayName, // Simpan siapa yang upload
             email: email, // Simpan email
             profilePicture: photoURL, // Simpan foto profil
           })
           .then((docRef) => {
-            alert("Document written with ID: " + docRef.id);
+            $("#barload").hide();
+            $("#modfinis").html(
+              "Upload ke server selesai...!!<br>silahkan upload foto lainnya"
+            );
+
+            $("#modfinis").show();
           })
           .catch((error) => {
-            alert("Error adding document: " + error);
+            console.log("Error adding document: " + error);
           });
-          
-          
       } else {
         alert("Image upload failed: ", result);
       }
@@ -198,43 +193,46 @@ var link  = "https://api.imgur.com/3/upload";
     });
 }
 
-
-
-function kirimdata(base64Imagena) {
-
+function kirimdataa(base64Imagena) {
   // Pisahkan base64 menjadi data biner (tanpa header MIME)
   const base64Image = base64Imagena.split(",")[1];
   const displayName = localStorage.getItem("displayName");
   const email = localStorage.getItem("email");
   const photoURL = localStorage.getItem("photoURL"); // Ambil URL foto profil
-  
-// Kirim menggunakan fetch
-fetch('https://twibone.bungtemin.net/dodol.php', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ image: base64Image })
-})
-.then(response => response.json())
-.then(data =>{ 
-       console.log(data.file);
-    db.collection("imageskoleksi")
-          .add({
-            imageUrl: data.file,
-            uploadedAt: new Date(),
-            uploadedBy: displayName, // Simpan siapa yang upload
-            email: email, // Simpan email
-            profilePicture: photoURL, // Simpan foto profil
-          })
-          .then((docRef) => {
-            console.log("Document written with ID: " + docRef.id);
-          })
-          .catch((error) => {
-            console.log("Error adding document: " + error);
-          });
-          
-})
-.catch(error => alert('Error:', error));
 
+  // Kirim menggunakan fetch
+  fetch("https://twibone.bungtemin.net/dodol.php", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ image: base64Image }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data.file);
+      db.collection("imageskoleksi")
+        .add({
+          imageUrl: data.file,
+          uploadedAt: new Date(),
+          uploadedBy: displayName, // Simpan siapa yang upload
+          email: email, // Simpan email
+          profilePicture: photoURL, // Simpan foto profil
+        })
+        .then((docRef) => {
+          console.log("Document written with ID: " + docRef.id);
+          $("#barload").hide();
+          $("#modfinis").html(
+            "Upload ke server selesai...!!<br>silahkan upload foto lainnya"
+          );
+
+          $("#modfinis").show();
+        })
+        .catch((error) => {
+          console.log("Error adding document: " + error);
+        });
+    })
+    .catch((error) => alert("Error:", error));
 }
+
+// Panggil fungsi ini untuk mengambil halaman pertama
---
+++ fotobingkai/script.js
@@ -0,0 +1,183 @@
+// Daftar gambar yang tersedia
+var images = [];
+for (var i = 1; i <= 19; i++) {
+  images.push("./images/FAMGET2024/" + i + ".png");
+}
+
+// Pilih gambar acak dari array
+var randomImage = images[Math.floor(Math.random() * images.length)];
+
+// Atur gambar acak sebagai src elemen img
+document.getElementById("imgfarmer").src = randomImage;
+
+const imgElement = document.getElementById("uploaded-image");
+let scale = 1;
+let lastScale = 1;
+
+const hammer = new Hammer(imgElement);
+
+hammer.get("pinch").set({ enable: true });
+
+hammer.on("pinch", function (ev) {
+  scale = lastScale * ev.scale;
+  imgElement.style.transform = `scale(${scale})`;
+});
+
+hammer.on("pinchend", function () {
+  lastScale = scale;
+});
+
+// batas
+
+var keys = { 37: 1, 38: 1, 39: 1, 40: 1 };
+function preventDefault(e) {
+  e.preventDefault();
+}
+
+function preventDefaultForScrollKeys(e) {
+  if (keys[e.keyCode]) {
+    preventDefault(e);
+    return false;
+  }
+}
+
+var supportsPassive = false;
+try {
+  window.addEventListener(
+    "test",
+    null,
+    Object.defineProperty({}, "passive", {
+      get: function () {
+        supportsPassive = true;
+      },
+    })
+  );
+} catch (e) {}
+
+var wheelOpt = supportsPassive ? { passive: false } : false;
+
+function disableScroll() {
+  window.addEventListener("touchmove", preventDefault, wheelOpt);
+  window.addEventListener("keydown", preventDefaultForScrollKeys, false);
+}
+
+function enableScroll() {
+  window.removeEventListener("touchmove", preventDefault, wheelOpt);
+  window.removeEventListener("keydown", preventDefaultForScrollKeys, false);
+}
+
+const position = { x: 0, y: 0 };
+
+interact("#uploaded-image").draggable({
+  listeners: {
+    move(event) {
+      position.x += event.dx;
+      position.y += event.dy;
+
+      // Terapkan posisi ke gambar
+      event.target.style.transform = `translate(${position.x}px, ${position.y}px) scale(${scale})`;
+      disableScroll();
+    },
+    end(event) {
+      enableScroll();
+    },
+  },
+});
+
+$(document).ready(function () {
+  var namafilenyaq =
+    localStorage.getItem("deviceIDi") + "_" + generateString(15);
+  $("#namaupload").val(namafilenyaq);
+  $("#hidden_datanama").val(namafilenyaq);
+  $("#hidden_datauser").val(localStorage.getItem("deviceIDi"));
+
+  $("input[name='FileOne']").on("change", function (event1) {
+    var file = event1.target.files[0];
+    var img = new Image();
+    $("#modfinis").hide();
+    img.onload = function () {
+      var originalWidth = img.width;
+      var originalHeight = img.height;
+
+      // Hitung rasio aspek
+      var frameWidth = $("#html-content-holder").width();
+      var frameHeight = $("#html-content-holder").height();
+      var frameAspectRatio = frameWidth / frameHeight;
+      var imageAspectRatio = originalWidth / originalHeight;
+
+      // Sesuaikan ukuran gambar berdasarkan rasio aspek
+      if (imageAspectRatio > frameAspectRatio) {
+        // Gambar lebih lebar daripada bingkai
+        var newWidth = frameWidth;
+        var newHeight = (newWidth / originalWidth) * originalHeight;
+      } else {
+        // Gambar lebih tinggi daripada bingkai
+        var newHeight = frameHeight;
+        var newWidth = (newHeight / originalHeight) * originalWidth;
+      }
+
+      // Set ukuran gambar
+      $("#uploaded-image").attr("src", URL.createObjectURL(file));
+      $("#uploaded-image").css({
+        width: newWidth + "px",
+        height: newHeight + "px",
+        transform: `translate(0px, 0px) scale(1)`,
+      });
+
+      position.x = 0; // Reset posisi
+      position.y = 0; // Reset posisi
+      scale = 1; // Reset skala
+      $("#btn_convert").show();
+      $("#customRange3").show();
+    };
+    img.src = URL.createObjectURL(file);
+  });
+
+  $("#customRange3").on("input", function () {
+    scale = 1 + $(this).val() / 100; // Skala gambar berdasarkan nilai slider
+    $("#uploaded-image").css(
+      "transform",
+      `translate(${position.x}px, ${position.y}px) scale(${scale})`
+    );
+  });
+  $(".pilihanimag").click(function () {
+    var gambarnew = $(this).attr("src");
+    var namafilenyag =
+      localStorage.getItem("deviceIDi") + "_" + generateString(15);
+    $("#namaupload").val(namafilenyag);
+    $("#hidden_datanama").val(namafilenyag);
+    $("#imgfarmer").attr("src", gambarnew);
+
+    var typenya = $(this).attr("data-id");
+    $("#hidden_datatype").val(typenya);
+  });
+
+  document
+    .getElementById("btn_convert")
+    .addEventListener("click", function (e) {
+      e.preventDefault();
+      $("#btn_convert").hide();
+      $("#barload").show();
+      $("#customRange3").hide();
+
+      html2canvas(document.getElementById("html-content-holder"), {
+        scale: 5, // Mengatur skala lebih tinggi untuk kualitas
+        allowTaint: true,
+        useCORS: true,
+      }).then(function (canvas) {
+        const imgData = canvas.toDataURL("image/jpeg", 1.0);
+        kirimdata(imgData);
+        var anchorTag = document.createElement("a");
+        //  var namafile = $("#namaupload").val() + ".jpg";
+        var namafile = "famget_" + generateString(5) + ".jpg";
+        document.body.appendChild(anchorTag);
+        anchorTag.download = namafile;
+        anchorTag.href = imgData;
+        anchorTag.click();
+
+        $("#modte").html(
+          "File telah di-download, silahkan cek di folder Download browser"
+        );
+      });
+    });
+});
